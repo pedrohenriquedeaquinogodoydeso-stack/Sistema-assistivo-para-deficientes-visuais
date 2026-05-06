be63fb6d91f2173d93d8e06fc8f1a0326c8ff2ca
Pedro <pedrohenriquedeaquinogodoydeso@gmail.com>
2026-05-06 14:48:39 -0300

    Implement YouTube mode and enhance command structure
    Added modes for YouTube interaction and improved command handling.

--- assistente2.py
+++ assistente2.py
@@ -13,7 +13,8 @@ from gtts import gTTS
 # CONFIG
 # =========================
 tasks = ""
-
+ex_mode = "normal"  # normal, youtube, 
+mode = "normal"  # normal, narrador, tarefas
 # =========================
 # VOZ
 # =========================
@@ -166,51 +167,141 @@ falar("Assistente iniciado")
 print("Assistente rodando...")
 
 while True:
+    
     comando = ouvir_comando()
+    if mode == "normal":
+        if not comando:
+            continue
 
-    if not comando:
-        continue
+        elif "clicar" in comando or "clica" in comando or "pausa" in comando or "despausa" in comando:
+            click()
 
-    elif "clicar" in comando or "clica" in comando or "pausa" in comando or "despausa" in comando:
-        click()
+        elif "hora" in comando:
+            exibir_hora()
 
-    elif "hora" in comando:
-        exibir_hora()
+        elif "volume" in comando:
+            volume()
 
-    elif "volume" in comando:
-        volume()
+        elif "abrir" in comando:
+            abrir()
 
-    elif "abrir" in comando:
-        abrir()
+        elif "escreva" in comando:
+            escrever()
 
-    elif "escreva" in comando:
-        escrever()
-
-    elif "pesquisar" in comando:
-        pesquisar()
-    elif "ler tudo" in comando or "ler tudo" in comando:
-        keyboard.press_and_release('control + a')
-        ler_selecao()
-    elif "ler tela" in comando or "narrador" in comando:
-        falar("Ativando narrador")
-        narrador() 
+        elif "pesquisar" in comando:
+            pesquisar()
+        elif "ler tudo" in comando or "ler tudo" in comando:
+            keyboard.press_and_release('control + a')
+            ler_selecao()
+        elif "ler tela" in comando or "narrador" in comando:
+            falar("Ativando narrador")
+            narrador() 
 
 
-    elif "ler seleção" in comando:
-        ler_selecao()
+        elif "ler seleção" in comando:
+            ler_selecao()
 
-    elif "onde estou" in comando:
-        janela_ativa()
+        elif "onde estou" in comando:
+            janela_ativa()
 
-    elif "adicionar tarefa" in comando:
-        adicionar_tarefa()
+        elif "adicionar tarefa" in comando:
+            adicionar_tarefa()
 
-    elif "ver tarefas" in comando or "missões" in comando:
-        listar_tarefas()
+        elif "ver tarefas" in comando or "missões" in comando:
+            listar_tarefas()
 
-    elif "remover tarefa" in comando:
-        remover_tarefa()
+        elif "remover tarefa" in comando:
+            remover_tarefa()
+        elif "youtube" in comando:# modo youtube
+            
+            keyboard.press_and_release('win')
+            time.sleep(0.5)
+            keyboard.write("Opera GX")
+            time.sleep(1)
+            keyboard.press_and_release('enter')
 
-    elif "sair" in comando or "parar" in comando:
-        falar("Encerrando assistente")
-        break
+            time.sleep(3)
+            keyboard.write("youtube.com")
+            keyboard.press_and_release('enter')
+            ex_mode = mode
+            mode = "youtube"
+            falar("Você está na página inicial do YouTube")
+            falar("Diga comandos para mais comandos relacionados ao YouTube, ou diga 'sair' para voltar ao modo normal")
+        elif "sair" in comando or "parar" in comando:
+            falar("Encerrando assistente")
+            raise SystemExit
+    elif mode == "youtube":
+        
+        
+        if "comandos" in comando:
+            falar("Comandos disponíveis: pesquisar, vídeos curtos, sair")
+            
+        elif "pesquisar" in comando:
+            falar("O que deseja pesquisar no YouTube?")
+            q = ouvir_comando()
+            keyboard.press_and_release('ctrl + l')
+            time.sleep(0.5)
+            keyboard.write("youtube.com/results?search_query=" + q.replace(" ", "+"))
+            keyboard.press_and_release('enter')
+            ex_mode = mode
+            falar("entrando no modo seleção de vídeos")
+            falar("Diga 'próximo' ou 'anterior' para navegar, e 'selecionar' para abrir o vídeo. Diga 'sair' para voltar ao modo normal")
+            mode = "seleção"
+            keyboard.press_and_release('ctrl + windows + enter')
+        elif "curtos" in comando or "vídeos curtos" in comando or "shorts" in comando:
+            keyboard.press_and_release('ctrl + l')
+            time.sleep(0.5)
+            keyboard.write("youtube.com/shorts")
+            keyboard.press_and_release('enter')
+            falar("Entrando no modo shorts")
+            mode = "shorts"
+
+        elif "sair" in comando or "parar" in comando:
+            mode = "normal"
+            falar("Voltando ao modo normal")
+    elif mode == "seleção":
+          # minimizar janela para facilitar navegação
+        if "próximo" in comando:
+            keyboard.press_and_release('tab')
+        elif "comandos" in comando:
+            falar("Comandos disponíveis: próximo, anterior, selecionar, sair")
+        elif "anterior" in comando:
+            keyboard.press_and_release('shift + tab')
+        elif "selecionar" in comando:
+            keyboard.press_and_release('enter')
+            falar("Vídeo selecionado. entrando no modo vídeo")
+            ex_mode = mode
+            mode = "vídeo"
+        elif "sair" in comando or "parar" in comando:
+            keyboard.press_and_release('ctrl + windows + enter')
+            if ex_mode == "youtube":
+                mode = "youtube"
+                falar("Voltando ao modo YouTube")
+            elif ex_mode == "normal":
+                mode = "normal"
+                falar("Voltando ao modo normal")
+    elif mode == "shorts":
+        if "próximo" in comando:
+            keyboard.press_and_release('down')  # seta para baixo
+        elif "comandos" in comando:
+            falar("Comandos disponíveis: próximo, anterior, pause, sair")
+        elif "anterior" in comando:
+            keyboard.press_and_release('up')  # seta para cima
+        elif "pause" in comando:
+            keyboard.press_and_release('space')
+        elif "sair" in comando or "parar" in comando:
+            mode = "youtube"
+            falar("Voltando ao modo YouTube")
+    elif mode == "vídeo":
+        if "pausar" in comando or "pause" in comando:
+            keyboard.press_and_release('space')
+        elif "comandos" in comando:
+            falar("Comandos disponíveis: pausar, sair")
+        elif "sair" in comando or "parar" in comando:
+            mode = "seleção"
+            falar("Voltando ao modo seleção de vídeos") 
+    #ADICIONAR TRADUÇÃO + modo vídeo com like, deslike, plkaylist, comentário
+    #cuidado com modo narrador para não ter dois narradores ativos ao mesmo tempo
+    #melhorar modo de pesquisa
+
+    
